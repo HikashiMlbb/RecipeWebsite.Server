eb6112f59c338115942d994107c1e353e1d20425
hikashi <hikashimlbb@gmail.com>
2024-11-03 21:43:44 +0700

    Improved Rating.cs behavior

--- src/RecipeWebsite.Domain/RecipeEntity/Rating.cs
+++ src/RecipeWebsite.Domain/RecipeEntity/Rating.cs
@@ -3,28 +3,17 @@ namespace RecipeWebsite.Domain.RecipeEntity;
 public class Rating
 {
     public int Rate => ComputeRate();
-
-    public List<int> Ratings;
-
-    public Rating()
-    {
-        Ratings = [0, 0, 0, 0, 0];
-    }
-
+    public int TotalRates { get; set; }
+    public int TotalVotes { get; set; }
+    
     private int ComputeRate()
     {
-        var rate = (int)Stars.One * Ratings[0] 
-                   + (int)Stars.Two * Ratings[1]
-                   + (int)Stars.Three * Ratings[2] 
-                   + (int)Stars.Four * Ratings[3] 
-                   + (int)Stars.Five * Ratings[4];
-        
-        var votes = Ratings[0] + Ratings[1] + Ratings[2] + Ratings[3] + Ratings[4];
-        return votes == 0 ? 0 : (int)Math.Round((float)rate / votes);
+        return TotalVotes == 0 ? 0 : (int)Math.Round((float)TotalRates / TotalVotes);
     }
 
     public void AddRate(Stars star)
     {
-        Ratings[(int)star - 1]++;
+        TotalRates += (int)star;
+        TotalVotes++;
     }
 }
